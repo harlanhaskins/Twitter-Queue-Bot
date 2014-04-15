5c0995d1c9ff94338f60dd3ea92fdec35c142446
Harlan Haskins <harlan@harlanhaskins.com>
2014-04-15 15:27:46 -0400

    Initial database and web api implementation.

---
+++ dbapi.py
@@ -0,0 +1,42 @@
+from peewee import * 
+db = SqliteDatabase('tweets.db', threadlocals=True)
+
+class Tweet(Model):
+    content = CharField()
+
+    class Meta():
+        database = db
+
+def addTweet(content="", shouldIncludeTag=False):
+    if not content:
+        return
+    if shouldIncludeTag:
+        content += " #420Puns"
+    Tweet.create(content=content)
+
+def allTweets():
+    return [user for user in Tweet.select().order_by(Tweet.id)]
+
+def numberOfTweets():
+    return Tweet.select().count()
+
+def topTweet():
+    tweets = (Tweet
+            .select()
+            .order_by(Tweet.id)
+            .limit(1))
+    if not tweets:
+        return None
+    return tweets.first()
+
+def popFirstTweet():
+    tweet = topTweet()
+    if not tweet:
+        return None
+    tweetQuery = (Tweet
+                .delete()
+                .where(Tweet.id == tweet.id))
+    tweetQuery.execute()
+    return tweet
+
+Tweet.create_table(True)
---
+++ webapi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from flask import Flask, Response
+from flask import request
+from flask.json import jsonify
+import argparse
+import dbapi
+
+app = Flask(__name__)
+
+@app.route("/add", methods=["POST"])
+def add():
+    arguments = request.args
+    tweet = arguments.get("tweet", "")
+    includeTag = arguments.get("includeTag", "")
+    if not (tweet):
+        return Response("Give me content, ya dingus.", status=412)
+
+    includeTag = not not includeTag
+
+    dbapi.addTweet(tweet, includeTag)
+    return Response("Added to the queue. That is number 42.", 200)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test",
+                        help="Runs the server in test mode and updates the testUsers database.",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    app.run(host="localhost", debug=args.test)
