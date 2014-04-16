ac2712e2e849da96ec6e00a8a5e5a01f8159da1d
Harlan Haskins <harlan@harlanhaskins.com>
2014-04-16 15:54:02 -0400

    Better CORS handling through an external library.

--- webapi.py
+++ webapi.py
@@ -5,54 +5,14 @@ from flask import request, make_response, request, current_app
 from flask.json import jsonify
 from functools import update_wrapper
 from datetime import timedelta
+from flask_cors import *
 import argparse
 import dbapi
 
 app = Flask(__name__)
 
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
 @app.route("/add", methods=["POST", "OPTIONS"])
-@crossdomain(origin='*')
+@cross_origin(headers=["Content-Type"])
 def add():
     if request.method == "OPTIONS":
         return Response("Yeah there's some options here, bro.", 200)
@@ -69,7 +29,7 @@ def add():
     return jsonify(tweet=tweetDict)
 
 @app.route("/next", methods=["GET"])
-@crossdomain(origin='*')
+@cross_origin(headers=["Content-Type"])
 def next():
     tweet = dbapi.topTweet()
     if not tweet:
@@ -77,17 +37,17 @@ def next():
     return jsonify(tweet=dbapi.dictionaryForTweet(tweet))
 
 @app.route("/count", methods=["GET"])
-@crossdomain(origin='*')
+@cross_origin(headers=["Content-Type"])
 def count():
     return jsonify(count=dbapi.numberOfTweets())
 
 @app.route("/all", methods=["GET"])
-@crossdomain(origin='*')
+@cross_origin(headers=["Content-Type"])
 def all():
     return jsonify(tweets=dbapi.allTweetDicts())
 
 @app.route("/remove", methods=["DELETE"])
-@crossdomain(origin='*')
+@cross_origin(headers=["Content-Type"])
 def remove():
     arguments = request.args
     id = arguments.get("id", "")
@@ -109,4 +69,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    app.run(host="localhost", debug=args.test)
+    app.run(host="localhost", port=4200, debug=args.test)
