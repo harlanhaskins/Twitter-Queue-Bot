5c718bf5c6efd6dda4a87c007fe15d23a0192714
Harlan Haskins <harlan@harlanhaskins.com>
2014-04-15 22:42:53 -0400

    Added tweet class that will tweet the top post if executed main.

---
+++ tweet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import requests
+import dbapi
+import json
+import webbrowser
+import twitter
+
+def tweet(twit):
+    tweet = dbapi.topTweet()
+    if not tweet:
+        return
+    twit.statuses.update(status=tweet.content)
+
+def urlWithEndpoint(endpoint):
+    return baseURL + endPoint
+
+def baseURL():
+    return "https://api.twitter.com/1.1/"
+
+def oAuthCredentials():
+    with open("credentials.txt") as credentialsFile:
+        return json.loads(credentialsFile.readline())
+
+def authenticate():
+    credentials        = oAuthCredentials()
+    consumerKey        = credentials["consumer_key"]
+    consumerSecret     = credentials["consumer_secret"]
+    accessKey          = credentials["access_key"]
+    accessSecret       = credentials["access_secret"]
+
+    auth = twitter.OAuth(accessKey, accessSecret, consumerKey, consumerSecret)
+    return twitter.Twitter(auth=auth)
+
+if __name__ == "__main__":
+    twit = authenticate()
+    tweet(twit)
