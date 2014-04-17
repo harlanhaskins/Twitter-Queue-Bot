589f0ec12b9abb58e0b9576511767603ada8ff3e
Harlan Haskins <harlan@harlanhaskins.com>
2014-04-16 20:03:45 -0400

    Utilizing the final solution for CORS stuff by @coalkids (https://github.com/coalkids/blog/blob/master/content/flask-cors.rst)

--- webapi.py
+++ webapi.py
@@ -11,8 +11,33 @@ import dbapi
 
 app = Flask(__name__)
 
+@app.before_request
+def option_autoreply():
+    """ Always reply 200 on OPTIONS request """
+
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
 @app.route("/add", methods=["POST", "OPTIONS"])
-@cross_origin(headers=["Content-Type"])
 def add():
     if request.method == "OPTIONS":
         return optionsResponse()
@@ -29,7 +54,6 @@ def add():
     return jsonify(tweet=tweetDict)
 
 @app.route("/next", methods=["GET"])
-@cross_origin(headers=["Content-Type"])
 def next():
     tweet = dbapi.topTweet()
     if not tweet:
@@ -37,20 +61,15 @@ def next():
     return jsonify(tweet=dbapi.dictionaryForTweet(tweet))
 
 @app.route("/count", methods=["GET"])
-@cross_origin(headers=["Content-Type"])
 def count():
     return jsonify(count=dbapi.numberOfTweets())
 
 @app.route("/all", methods=["GET"])
-@cross_origin(headers=["Content-Type"])
 def all():
     return jsonify(tweets=dbapi.allTweetDicts())
 
 @app.route("/remove", methods=["DELETE", "OPTIONS"])
-@cross_origin(headers=["Content-Type"])
 def remove():
-    if request.method == "OPTIONS":
-        return optionsResponse()
     arguments = request.args
     id = arguments.get("id", "")
     if not id:
@@ -60,6 +79,18 @@ def remove():
         return databaseErrorResponse()
     tweetDict = dbapi.dictionaryForTweet(response)
     return jsonify(tweet=tweetDict)
+
+@app.after_request
+def set_allow_origin(resp):
+    """ Set origin for GET, POST, PUT, DELETE requests """
+
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if request.method != 'OPTIONS' and 'Origin' in request.headers:
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+    return resp
 
 def databaseErrorResponse():
     return Response("The database is giving some issues with that query.", 500)
